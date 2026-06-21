18a066cd0135a3b620d16af847c8fc6fa04671cf
dalidoshx <adi.dancau@gmail.com>
2026-06-21 01:49:14 +0100

    Add variables and template literal exercise

---
+++ exercises/00-variables.js
@@ -0,0 +1,17 @@
+// TODO: 1. Declare a const for your name (string)
+// TODO: 2. Declare a const for your age (number)
+// TODO: 3. Declare a let for whether you're currently coding right now (boolean)
+// TODO: 4. console.log each variable on its own line
+// TODO: 5. console.log one sentence using a template literal that uses all three variables
+
+const name = "Adrian";
+const age = 38;
+let isCoding = true;
+
+console.log(name);
+console.log(age);
+console.log(isCoding);
+
+console.log(
+  `My name is ${name}, I am ${age} years old, and it is ${isCoding} that I am currently coding right now.`,
+);
